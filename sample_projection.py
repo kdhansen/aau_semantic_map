9dcb85ee89072b460c1b6443a7c52a7ef39b745f
Karl Damkjær Hansen <kdh@es.aau.dk>
2021-06-10 22:50:48 +0200

    Rearange into module

---
+++ sample_projection.py
@@ -0,0 +1,92 @@
+from semanticmap.HomographicProjection import HomographicProjection
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+import matplotlib.pyplot as plt
+
+def set_axes_equal(ax):
+    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+    '''
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])*1.5
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])*1.5
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])*1.5
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
+if __name__ == "__main__":
+    """Show a camera frame as a projected bounding box on a plane
+
+    Several frames are used:
+        W - World
+        C - Camera
+        P - Plane (The one we are projecting to.)
+    """
+    # Transformation from world frame to camera-image frame. Our camera is
+    # raised 4 m along the z axis In this experiment, the camera is pointing 1.5
+    # radians down towards the ground
+    camera_transform = np.identity(4)
+    camera_transform[0:3, 3] = [0, 0, 4]
+    rot_C = R.from_rotvec(1.3 * np.array([0, 1, 0]))
+    camera_transform[0:3, 0:3] = rot_C.as_matrix()
+
+    # The camera is a 640x480
+    camera_matrix = np.array([[250.,   0., 320.],
+                              [  0., 250., 240.],
+                              [  0.,   0.,   1.]])
+
+    # Plane tranform. The plane is raised 1 m along the z axis and rotated
+    # slightly around x.
+    plane_transform = np.identity(4)
+    plane_transform[0:3, 3] = [0, 0, 1]
+    rot_P = R.from_rotvec(0.1 * np.array([1, 0, 0]))
+    plane_transform[0:3, 0:3] = rot_P.as_matrix()
+
+    # Constructing homographies for both the plane and the ground.
+    H_ground = HomographicProjection(camera_transform, np.identity(4), camera_matrix)
+    H_plane = HomographicProjection(camera_transform, plane_transform, camera_matrix)
+
+    # Outline of the frame
+    pixels = [(0, 0), (0, 480), (640, 480), (640, 0)]
+    camera_frame_box = []
+    ground_points = []
+    plane_points = []
+    for p in pixels:
+        camera_frame_box.append(H_plane.pixel_in_image_plane(p))
+        ground_points.append(H_ground.project_to_plane(p))
+        plane_points.append(H_plane.project_to_plane(p))
+
+    # Plot it all
+    fig = plt.figure()
+    ax =  fig.add_subplot(111, projection='3d')
+    ax.plot(*zip(*camera_frame_box))
+    ax.plot(*camera_transform[0:3, 3], '*')
+    ax.plot(*zip(*ground_points))
+    ax.plot(*zip(*plane_points))
+
+    test_point = [200,200]
+    tp_C = H_plane.pixel_in_image_plane(test_point)
+    tp_P = H_plane.project_to_plane(test_point)
+    ax.plot(*tp_C, '*')
+    ax.plot(*tp_P, '*')
+
+    set_axes_equal(ax)
+
+    plt.show()
--- semanticmap/HomographicProjection.py
+++ semanticmap/HomographicProjection.py
@@ -1,8 +1,6 @@
 import numpy as np
 from numpy import pi
 from numpy.linalg import inv, multi_dot
-from scipy.spatial.transform import Rotation as R
-import matplotlib.pyplot as plt
 
 class HomographicProjection:
     """Homographic projection of a pixel in a camera to a 2D plane"""
@@ -115,84 +113,3 @@ class HomographicProjection:
         pixel_I[0:3] = self._T_IS.dot(pixel_in_sensor)
         pixel_W = self._T_WC.dot(self._T_CI.dot(pixel_I))
         return tuple(pixel_W[0:3])
-
-def set_axes_equal(ax):
-    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
-    cubes as cubes, etc..  This is one possible solution to Matplotlib's
-    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
-
-    Input
-      ax: a matplotlib axis, e.g., as output from plt.gca().
-    '''
-
-    x_limits = ax.get_xlim3d()
-    y_limits = ax.get_ylim3d()
-    z_limits = ax.get_zlim3d()
-
-    x_range = abs(x_limits[1] - x_limits[0])*1.5
-    x_middle = np.mean(x_limits)
-    y_range = abs(y_limits[1] - y_limits[0])*1.5
-    y_middle = np.mean(y_limits)
-    z_range = abs(z_limits[1] - z_limits[0])*1.5
-    z_middle = np.mean(z_limits)
-
-    # The plot bounding box is a sphere in the sense of the infinity
-    # norm, hence I call half the max range the plot radius.
-    plot_radius = 0.5*max([x_range, y_range, z_range])
-
-    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
-    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
-    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
-
-if __name__ == "__main__":
-    ### Transformation from localization frame to camera-image frame ###
-    # Frame P is localization plane-frame, C is camera frame, I is image plane, 
-    # which is oriented z along line of sight, x right and y down.
-    camera_transform = np.identity(4)
-    # Camera is raised 4 m along the z axis
-    camera_transform[0:3, 3] = [0, 0, 4]
-    # In this experiment, the camera is pointing 1.5 radians down towards the ground
-    rot_C = R.from_rotvec(1. * np.array([0, 1, 0]))
-    camera_transform[0:3, 0:3] = rot_C.as_matrix()
-
-    # The camera is a 640x480
-    camera_matrix = np.array([[250.,   0., 320.],
-                              [  0., 250., 240.],
-                              [  0.,   0.,   1.]])
-
-    # plane tranform
-    # plane is raised 1 m along the z axis and rotated slightly around x.
-    plane_transform = np.identity(4)
-    plane_transform[0:3, 3] = [0, 0, 1]
-    rot_P = R.from_rotvec(0.2 * np.array([1, 0, 0]))
-    plane_transform[0:3, 0:3] = rot_P.as_matrix()
-
-    H_ground = HomographicProjection(camera_transform, np.identity(4), camera_matrix)
-    H_plane = HomographicProjection(camera_transform, plane_transform, camera_matrix)
-
-    # outline of the frame
-    pixels = [(0, 0), (0, 480), (640, 480), (640, 0)]
-    camera_frame_box = []
-    ground_points = []
-    plane_points = []
-    for p in pixels:
-        camera_frame_box.append(H_plane.pixel_in_image_plane(p))
-        ground_points.append(H_ground.project_to_plane(p))
-        plane_points.append(H_plane.project_to_plane(p))
-
-    fig = plt.figure()
-    ax =  fig.add_subplot(111, projection='3d')
-    ax.plot(*zip(*camera_frame_box))
-    ax.plot(*camera_transform[0:3, 3], '*')
-    ax.plot(*zip(*ground_points))
-    ax.plot(*zip(*plane_points))
-
-    test_point = [200,200]
-    tp_C = H_plane.pixel_in_image_plane(test_point)
-    tp_P = H_plane.project_to_plane(test_point)
-    ax.plot(*tp_C, '*')
-    ax.plot(*tp_P, '*')
-
-    set_axes_equal(ax)
-
-    plt.show()
