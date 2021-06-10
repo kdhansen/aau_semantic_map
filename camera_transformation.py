b19ef0e44cf5bf9d9f5d8f743658f45a725603d7
Karl Damkjær Hansen <kdh@es.aau.dk>
2021-06-10 22:04:16 +0200

    Refactor into class

--- camera_transformation.py
+++ camera_transformation.py
@@ -4,110 +4,117 @@ from numpy.linalg import inv, multi_dot
 from scipy.spatial.transform import Rotation as R
 import matplotlib.pyplot as plt
 
-def homography_projection(pixel, camera_transform, plane_transform, camera_intrinsics):
-    """Compute the homographic projection of a pixel from a camera to a plane.
-
-    Given the world transformations of a camera and a projection plane, this
-    computes the homography that projects a pixel in the image plane to a point
-    on the projection plane.
+class HomographicProjection:
+    """Homographic projection of a pixel in a camera to a 2D plane"""
     
-    The camera world transformation is a 4x4 SE(3) transformation from the world
-    frame to the camera frame, before applying camera intrinsics.
+    def __init__(self, camera_transform, plane_transform, camera_intrinsics):
+        """Compute the homographic projection of a pixel from a camera to a
+        plane.
 
-    The plane world transform is also an SE(3). If the pixel is projected to the
-    world frame, this transform is just the identity matrix, e.g.:
+        Given the world transformations of a camera and a projection plane, this
+        computes the homography that projects a pixel in the image plane to a
+        point on the projection plane.
+
+        The camera world transformation is a 4x4 SE(3) transformation from the
+        world frame to the camera frame, before applying camera intrinsics.
+
+        The plane world transform is also an SE(3). If the pixel is projected to
+        the world frame, this transform is just the identity matrix, e.g.:
         np.identity(4).
-    
-    All three matrices must be passed as Numpy arrays.
-    
-    The camera intrinsics are dependent on the camera hardware, usually
-    available from the camera metadata or from calibration.
 
-    The returned point is given in world coordinates.
+        All three matrices must be passed as Numpy arrays.
 
-    See more in the paper: "Robot Cigarette-Litter Identification and Removal"
+        The camera intrinsics are dependent on the camera hardware, usually
+        available from the camera metadata or from calibration.
 
-    Example:
-        The camera is 1.5 meters above ground and pointed 45 degrees downwards,
-        the pixel (323,205) is projected to the world plane. The used camera is
-        a 640x480 px with a focal length of 257 px. 
+        See more in the paper: "Robot Cigarette-Litter Identification and
+        Removal"
+
+        Example:
+            The camera is 1.5 meters above ground and pointed 45 degrees
+            downwards, the pixel (323,205) is projected to the world plane. The
+            used camera is a 640x480 px with a focal length of 257 px. 
+            
+            cam_transform = np.identity(4)
+            cam_transform[0:3, 3] = [0, 0, 1.5]
+            cam_transform[0:3, 0:3] = np.array([[  cos(45),  0,  sin(45)],
+                                                [        0,  0,        0],
+                                                [ -sin(45), -1,  cos(45)]])
+            plane_transform = np.identity(4)
+            intrinsics = np.array([[257,   0, 320],
+                                   [  0, 257, 240],
+                                   [  0,   0,   1]])
+            H = HomographicProjection(cam_transform, plane_transform, intrinsics)
+
+            point_in_plane = H.project_to_plane([323,205])
+
+        Note: The rotation matrix here breaks the syntax a bit. Both numpy.cos
+            and math.cos use radians. You could use
+            scipy.spatial.transform.Rotation to generate the matrix.
+        """
+
+        # Transformations
+        # Just recasting to T_xx notation. T_WC is the transformation of the camera
+        # frame in the world frame, and T_WP is the transformation of the projecting
+        # plane in the world frame.
+        self._T_WC = camera_transform
+        self._T_WP = plane_transform
+
+        # The image plane, I, is oriented to point z along the line of sight,
+        # x right and y down seen from the camera frame, C.
+        self._T_CI = np.array([[ 0,  0,  1, 0],
+                        [-1,  0,  0, 0],
+                        [ 0, -1,  0, 0],
+                        [ 0,  0,  0, 1]])
+
+        # The camera intrinsics is the transformation from a point in the image
+        # plane to a point in the sensor plane. This is opposite the direction we
+        # want, so we invert it.
+        self._T_IS = inv(camera_intrinsics)
+
+        # Convert the camera focal point to the projection plane frame through
+        # the frames camera->world->projection plane (f_I->f_P). The focal
+        # point, f_I, is is the origin in the image frame.
+        self._T_PI = multi_dot([inv(self._T_WP), self._T_WC, self._T_CI])
         
-        cam_transform = np.identity(4)
-        cam_transform[0:3, 3] = [0, 0, 1.5]
-        cam_transform[0:3, 0:3] = np.array([[  cos(45),  0,  sin(45)],
-                                            [        0,  0,        0],
-                                            [ -sin(45), -1,  cos(45)]])
-        intrinsics = np.array([[257,   0, 320],
-                               [  0, 257, 240],
-                               [  0,   0,   1]])
-        point_in_plane = homography_projection([323,205], cam_transform,
-                                               np.identity(4), intrinsics)
+        self._f_I = np.array([0., 0., 0., 1.])
+        self._f_P = self._T_PI.dot(self._f_I)
 
-    Note: The rotation matrix here breaks the syntax a bit. Both numpy.cos and
-          math.cos use radians. You could use scipy.spatial.transform.Rotation
-          to generate the matrix.
-    """
-    # Transformations
-    # Just recasting to T_xx notation. T_WC is the transformation of the camera
-    # frame in the world frame, and T_WP is the transformation of the projecting
-    # plane in the world frame.
-    T_WC = camera_transform
-    T_WP = plane_transform
+    def project_to_plane(self, pixel):
+        """Project a pixel to the plane.
 
-    # The image plane, I, is oriented to point z along the line of sight,
-    # x right and y down seen from the camera frame, C.
-    T_CI = np.array([[ 0,  0,  1, 0],
-                     [-1,  0,  0, 0],
-                     [ 0, -1,  0, 0],
-                     [ 0,  0,  0, 1]])
+        The returned point is given in world coordinates.
+        """
+        
+        # Convert pixel projection plane frame. The pixel is given in the sensor
+        # frame (homogeneous 2D) (r_S), so this is first transformed into the
+        # homogeneous 3D image plane frame (r_I). Then the pixel is converted
+        # through the frames camera->world->projection plane (r_I->r_P).
+        r_S = np.array([pixel[0], pixel[1], 1])
+        r_I = np.ones(4)
+        r_I[0:3] = self._T_IS.dot(r_S)
+        r_P = self._T_PI.dot(r_I)
 
-    # The camera intrinsics is the transformation from a point in the image
-    # plane to a point in the sensor plane. This is opposite the direction we
-    # want, so we invert it.
-    T_IS = inv(camera_intrinsics)
+        # The distance scalar use only the z-components of r and f. 
+        z_fP = self._f_P[2]
+        z_rP = r_P[2]
+        d = z_fP / (z_fP - z_rP)
 
-    # Convert pixel and camera focal point to the projection plane frame. The
-    # pixel is given in the sensor frame (homogeneous 2D) (r_S), so this is
-    # first transformed into the homogeneous 3D image plane frame (r_I). Then
-    # both the pixel and focal point is converted through the frames
-    # camera->world->projection plane (r_I->r_P and f_I->f_P). The focal point,
-    # f_I, is is the origin in the image frame.
-    T_PI = multi_dot([inv(T_WP), T_WC, T_CI])
-    
-    f_I = np.array([0., 0., 0., 1.])
-    f_P = T_PI.dot(f_I)
+        # Scaling the pixel in P to project it onto the P plane.
+        p_P = self._f_P + d*(r_P-self._f_P)
 
-    r_S = np.array([pixel[0], pixel[1], 1])
-    r_I = np.ones(4)
-    r_I[0:3] = T_IS.dot(r_S)
-    r_P = T_PI.dot(r_I)
-
-    # The distance scalar use only the z-components of r and f. 
-    z_fP = f_P[2]
-    z_rP = r_P[2]
-    d = z_fP / (z_fP - z_rP)
-
-    # Scaling the pixel in P to project it onto the P plane.
-    p_P = f_P + d*(r_P-f_P)
-
-    # Returning the point in world frame as a regular list (not homogeneous) of three coordinates.
-    p_W = T_WP.dot(p_P)
-    print("p_P:", p_P)
-    print("T_WP: ", T_WP)
-    print("p_W:", p_W)
-    return list(p_W[0:3])
+        # Returning the point in world frame as a regular list (not homogeneous) of three coordinates.
+        p_W = self._T_WP.dot(p_P)
+        return list(p_W[0:3])
     
 
-def pixel_in_image_plane(pixel, camera_matrix, camera_transform):
-    pixel_in_sensor = np.array([pixel[0], pixel[1], 1])
-    pixel_I = np.ones(4)
-    pixel_I[0:3] = inv(camera_matrix).dot(pixel_in_sensor)
-    T_CI = np.array([[ 0,  0,  1, 0],
-                     [-1,  0,  0, 0],
-                     [ 0, -1,  0, 0],
-                     [ 0,  0,  0, 1]])
-    pixel_W = camera_transform.dot(T_CI.dot(pixel_I))
-    return tuple(pixel_W[0:3])
+    def pixel_in_image_plane(self, pixel):
+        """Get the pixel in the image frame in world coordinates."""
+        pixel_in_sensor = np.array([pixel[0], pixel[1], 1])
+        pixel_I = np.ones(4)
+        pixel_I[0:3] = self._T_IS.dot(pixel_in_sensor)
+        pixel_W = self._T_WC.dot(self._T_CI.dot(pixel_I))
+        return tuple(pixel_W[0:3])
 
 def set_axes_equal(ax):
     '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
@@ -145,7 +152,7 @@ if __name__ == "__main__":
     # Camera is raised 4 m along the z axis
     camera_transform[0:3, 3] = [0, 0, 4]
     # In this experiment, the camera is pointing 1.5 radians down towards the ground
-    rot_C = R.from_rotvec(1.5 * np.array([0, 1, 0]))
+    rot_C = R.from_rotvec(1. * np.array([0, 1, 0]))
     camera_transform[0:3, 0:3] = rot_C.as_matrix()
 
     # The camera is a 640x480
@@ -160,6 +167,8 @@ if __name__ == "__main__":
     rot_P = R.from_rotvec(0.2 * np.array([1, 0, 0]))
     plane_transform[0:3, 0:3] = rot_P.as_matrix()
 
+    H_ground = HomographicProjection(camera_transform, np.identity(4), camera_matrix)
+    H_plane = HomographicProjection(camera_transform, plane_transform, camera_matrix)
 
     # outline of the frame
     pixels = [(0, 0), (0, 480), (640, 480), (640, 0)]
@@ -167,9 +176,9 @@ if __name__ == "__main__":
     ground_points = []
     plane_points = []
     for p in pixels:
-        camera_frame_box.append(pixel_in_image_plane(p, camera_matrix , camera_transform))
-        ground_points.append(homography_projection(p, camera_transform, np.identity(4), camera_matrix))
-        plane_points.append(homography_projection(p, camera_transform, plane_transform, camera_matrix))
+        camera_frame_box.append(H_plane.pixel_in_image_plane(p))
+        ground_points.append(H_ground.project_to_plane(p))
+        plane_points.append(H_plane.project_to_plane(p))
 
     fig = plt.figure()
     ax =  fig.add_subplot(111, projection='3d')
@@ -179,8 +188,8 @@ if __name__ == "__main__":
     ax.plot(*zip(*plane_points))
 
     test_point = [200,200]
-    tp_C = pixel_in_image_plane(test_point, camera_matrix , camera_transform)
-    tp_P = homography_projection(test_point, camera_transform, plane_transform, camera_matrix)
+    tp_C = H_plane.pixel_in_image_plane(test_point)
+    tp_P = H_plane.project_to_plane(test_point)
     ax.plot(*tp_C, '*')
     ax.plot(*tp_P, '*')
 
